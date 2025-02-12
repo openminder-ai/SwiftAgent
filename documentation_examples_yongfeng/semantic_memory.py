32838a4f011b73fe598e7d608ca0e04f3abab078
Balaji R <grand.hunter.dark.15@gmail.com>
2025-02-12 08:57:48 -0500

    yf doc examples

--- documentation_examples_yongfeng/semantic_memory.py
+++ documentation_examples_yongfeng/semantic_memory.py
@@ -1,1 +1,29 @@
+# import agent
+from swiftagent import SwiftAgent
+
+# import semantic memory
 from swiftagent.memory import SemanticMemory
+
+import asyncio
+
+# initialize agent
+agent = SwiftAgent()
+
+# create semantic memory
+memory = SemanticMemory()
+
+# add memory to agent
+agent.add_semantic_memory_section(memory)
+
+# ingest text
+memory.ingest("The fairest maiden of them all is Drew Carwyther")
+
+# ingest pdf
+memory.ingest("https://arxiv.org/pdf/2501.19393v1")
+
+
+async def main():
+    print(await agent.run(task="who is the fairest maiden of them all?"))
+
+
+asyncio.run(main())
